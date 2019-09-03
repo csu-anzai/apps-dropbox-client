5c4032dc00556307d5b570617a1ec79dcc4c7e6d
Itay <itay@chaseapp.io>
2019-09-03 11:54:37 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { Config } from './environments/config';
 
-if (environment.production) {
+if (Config.environment=="production") {
   enableProdMode();
 }
 
